bf990dd5e55bd2c6a4af0651a6c8a8eaae8a1e24
tarcizio lima <tarcizio.lima@fpf.br>
2026-09-01 11:59:51 -0400

    ajustando o teste do titulo e criando teste de slogan

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -1,10 +1,15 @@
 import { test, expect } from '@playwright/test'
 
-test('deve exibir o slogan na homepage', async ({ page }) => {
+test('deve validar o titulo da aba do navegador', async ({ page }) => {
   await page.goto('http://localhost:3000/')
 
   await expect(page).toHaveTitle("Lunar Pass — Passagens para a Lua")
 })
 
+test('deve exibir o slogan na homepage', async ({ page }) => {
+  await page.goto('http://localhost:3000/')
 
+  const slogan = page.getByRole('heading', { name: 'Sua viagem para a Lua começa aqui.' })
 
+  await expect(slogan).toBeVisible()
+})
